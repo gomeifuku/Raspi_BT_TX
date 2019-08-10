a782d066b30ca8b8d77f621946012ec1b17b8e3a
gomefuku <junniku33@gmail.com>
2019-08-10 17:33:29 +0900

    added LR Switch

--- arduino_rx/main_controller/RN4020.h
+++ arduino_rx/main_controller/RN4020.h
@@ -1,5 +1,5 @@
 void Initialize();
 void Check_CMD();
-void Register_Private_Service(String uuid);;
+void Register_Private_Service(String uuid);
 void Register_Private_Characteristic(String uuid, int property, int bytes);
 int getBLEAccess();
--- arduino_rx/main_controller/main_controller.ino
+++ arduino_rx/main_controller/main_controller.ino
@@ -1,12 +1,20 @@
 #include "RN4020.h"
 #include "key_settings.h"
+
+volatile int isLightON = 0;
+
 void setup() {
-  pinMode(PIN_TEST_LED, OUTPUT);
   pinMode(PIN_IN1, OUTPUT);
   pinMode(PIN_IN2, OUTPUT);
+  pinMode(PIN_FOWARD_STOP, INPUT);
+  pinMode(PIN_REVERSE_STOP, INPUT);
+  pinMode(PIN_DBG_LED1, OUTPUT);
   Initialize();
+  attachInterrupt(0, TriggerLigthOn, FALLING);
+  attachInterrupt(1, TriggerLigthOff, FALLING);
   // 回転速度調整
   analogWrite(PIN_VREF, 127);
+  /*Light OFFの状態までモータを回す*/
 }
 
 void loop() {
@@ -17,34 +25,52 @@ void loop() {
 //RN4020 書き込みコマンド受信ポーリング
 void pollingBTcmd() {
   int ret=getBLEAccess();
-  if(ret<0){
-    if(ret==1){
-      Trigger_ON();
-    }else{
+  if(ret==1){
+    if(isLightON){
       Trigger_OFF();
+    }else{
+      Trigger_ON();      
     }
-    Serial.print("!GET FOUND!");
   }
 }
-
+/*モータ正転*/
+void RotateFoward(){
+  digitalWrite(PIN_IN1, HIGH);
+  digitalWrite(PIN_IN2, LOW);
+}
+/*モータ逆転*/
+void RotateReverse(){
+  digitalWrite(PIN_IN1, LOW);
+  digitalWrite(PIN_IN2, HIGH);
+}
+/*モータ正転*/
+void RotateStop(){
+  digitalWrite(PIN_IN1, HIGH);
+  digitalWrite(PIN_IN2, HIGH);
+}
 
 void Trigger_ON() {
   // 回転
-  digitalWrite(PIN_IN1, HIGH);
-  digitalWrite(PIN_IN2, LOW);
+  RotateFoward();
   delay(5000);
-  // ブレーキ
-  digitalWrite(PIN_IN1, HIGH);
-  digitalWrite(PIN_IN2, HIGH);
+  // SANITY::ブレーキ
+  RotateStop();
 }
 
 void Trigger_OFF() {
   // 逆回転
-  digitalWrite(PIN_IN1, LOW);
-  digitalWrite(PIN_IN2, HIGH);
+  RotateReverse();
   delay(5000);
-  // ブレーキ
-  digitalWrite(PIN_IN1, HIGH);
-  digitalWrite(PIN_IN2, HIGH);
+  // SANITY::ブレーキ
+  RotateStop();
+}
+
+void TriggerLigthOn(){
+  isLightON=1;
+  RotateStop();
+}
+void TriggerLigthOff(){
+  isLightON=0;
+  RotateStop();
 }
 
